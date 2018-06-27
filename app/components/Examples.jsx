8e19f6ed53820626decc9267a36d29961265d6a1
anirudhparashar <anirudh@atlogys.com>
2018-06-27 17:27:47 +0530

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Examples = React.createClass({
   render: function () {
     return (
+      <div>
+        <h3>Change</h3>
       <h3>Examples Component!</h3>
+      </div>
     )
   }
 });
